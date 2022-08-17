8470830e58532a2c36d25b59fc1c50bbcb4589cb
Jacob <jacobsimoninielsen@gmail.com>
2022-08-17 13:59:29 +0200

    Hide Form

--- js/events.js
+++ js/events.js
@@ -2,15 +2,19 @@
 const cow = document.getElementById("cow");
 const pig = document.getElementById("pig");
 const moose = document.getElementById("moose");
+const form = document.getElementById("form")
+const body = document.getElementById("backgroundColor")
 // validate Buttons for form
 const validatePig = document.getElementById("evaluatePig");
-const validateAll = document.getElementById("evaluateForm")
+const validateAll = document.getElementById("evaluateForm");
+const fjernForm = document.getElementById("remove");
 
 //Eventlisteners
 
 cow.addEventListener("keyup",evaluateCow)
 validatePig.addEventListener("click",evaluatePig)
 validateAll.addEventListener("click",evaluateAll)
+fjernForm.addEventListener("click",removeForm)
 
 //Functions ------------------------------------------------------
 function evaluateCow() {
@@ -41,7 +45,7 @@ function evaluateAll() {
         console.log("WUUU Cow");
     } else {
         cow.style.backgroundColor = "red"
-        console.log("No Cow");
+        console.log("No Cow");        
     }
 
     if(pig.value == "gris") {
@@ -59,5 +63,40 @@ function evaluateAll() {
         moose.style.backgroundColor = "red"
         console.log("No Moose");
     }
+
+
+    var text;
+    var dyr = document.getElementById("select").value;
+  
+    switch(dyr) {
+        //case er value i options
+      case "fugl":
+        text = "Fugle er seje";
+      break;
+
+      case "fisk":
+      text = "Fisk drikker vand";
+      break;
+
+      case "reptil":
+      text = "Nogen kan skifte farve";
+      break;
+      
+      case "insekt":
+      text = "Ad";
+      break;
+
+      case "pattedyr":
+      text = "De ligger ikke æg (Næbdyr xd)";
+      break;
+
+      default:
+      text = "What?";
+    }
+    document.getElementById("text").innerHTML = text;
+}
+
+function removeForm() {
+    form.style.display = "none";
 }
 //Function -------------------------------------------------------
